304346890738984c21b72937eafb83f4799193c9
Darrell-Conklin <dmconklin84@gmail.com>
2020-06-10 19:47:26 -0500

    added another env var

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -5,6 +5,7 @@ const router = express.Router()
 router.get("/", (req, res) => {
 	res.status(200).json({
 		message: `Welcome ${process.env.COHORT}`,
+		fun_fact: process.env.FUN_FACT || "No fun"
 	})
 })
 
